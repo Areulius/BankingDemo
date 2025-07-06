4fba95a57dac0cfdc22422c4bb4e2cef8dc16f55
Valdis Arelis <arelisvaldis@gmail.com>
2025-07-06 22:07:23 +0300

    commented out unnecesasry colors, updated mainapp a little

---
+++ Banking/src/BankUser.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import static java.lang.Math.round;
+
+public class BankUser {
+    private String username;
+    private int id;
+    ArrayList<BankAccount> UserAccounts;
+
+    public BankUser (){
+        //this.id = (int)round(100*Math.random());
+    }
+}
--- Banking/src/MainApp.java
+++ Banking/src/MainApp.java
@@ -1,21 +1,34 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
-public class MainApp {
+
+public class MainApp implements ConsoleColors{
     public static void main(String[] args) {
+
+        // create list of users
+        ArrayList<BankUser> UserList = new ArrayList<>();
+
+        // start terminal program
         try {
             BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
 
+            System.out.println(GREEN_BOLD + "------------------------" + RESET);
+            System.out.println(GREEN_BOLD_BRIGHT + "| WELCOME TO THE BANK™ |" + RESET);
+            System.out.println(GREEN_BOLD + "------------------------" + RESET);
+            System.out.println(GREEN + "Login with 'login <username>'" + RESET);
+            System.out.println(GREEN + "See all commands with 'help'" + RESET);
 
             boolean exit = false;
             while(!exit) {
+                System.out.print(">> ");
                 String[] command = r.readLine().split(" +");
                 switch (command[0]) {
                     case "help":
-                        System.out.println(ConsoleColors.BLACK_BOLD + "help command wip");
+                        System.out.println("help command wip");
                         break;
-                    case "hello":
-                        System.out.println("Hello, user");
+                    case "login":
+                        System.out.println("login functionality wip");
                         break;
                     case "exit":
                         exit = true;
